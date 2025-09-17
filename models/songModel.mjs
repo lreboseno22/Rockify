bc58f42929e1eec6cfcdfa556f51944fd9ba3472
Liam <77292172+lreboseno22@users.noreply.github.com>
2025-09-17 14:41:26 -0400

    create song model schema

--- models/songModel.mjs
+++ models/songModel.mjs
@@ -1,7 +1,34 @@
 import mongoose from "mongoose";
 
 const songSchema = new mongoose.Schema({
-
+    title: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    artist: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Artist",
+        required: true
+    },
+    album: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: " Album",
+        required: true
+    },
+    duration: {
+        type: Number,
+        required: true
+    },
+    audioFile: {
+        type: String,
+        required: true,
+        trim: true
+    }, 
+    genre: {
+        type: String,
+        default: "Rock",
+    }
 })
 
 const Song = mongoose.model("Song", songSchema);
