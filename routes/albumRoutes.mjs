94924860cf2d029b4bd85ef6d5be95f406e77bcc
Liam <77292172+lreboseno22@users.noreply.github.com>
2025-09-19 11:21:01 -0400

    full CRUD on albums

--- routes/albumRoutes.mjs
+++ routes/albumRoutes.mjs
@@ -24,6 +24,43 @@ router.get("/:id", async (req, res) => {
     }
 });
 
+// Post new album
+router.post("/:id", async (req, res) => {
+    try {
+        const newAlbum = new Album(req.body);
+        const savedAlbum = await newAlbum.save();
+        res.status(201).json({ savedAlbum });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
 
+// Put update album
+router.put("/:id", async (req, res) => {
+    try {
+        const updatedAlbum = await Album.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            { new: true, runValidators: true }
+        );
+
+        if (!updatedAlbum) return res.status(404).json({ error: "Album not found" });
+        res.json(updatedAlbum);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+})
+
+
+// Delete album
+router.delete("/:id", async (req, res) => {
+    try {
+        const deletedAlbum = await Album.findByIdAndDelete(req.params.id);
+        if(!deletedAlbum) return res.status(404).json({ error: "Album not found" });
+        res.json({ message: "Album deleted" });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
 
 export default router;
