30f1892d518b1243dcbd8316daa4b4877a55b892
Liam <77292172+lreboseno22@users.noreply.github.com>
2025-09-19 14:12:02 -0400

    playlist CRUD routes complete

--- routes/playlistRoutes.mjs
+++ routes/playlistRoutes.mjs
@@ -24,8 +24,46 @@ router.get("/:id", async (req, res) => {
         if(!playlist) return res.status(404).json({ error: "Playlist not found" });
         res.json(playlist);
     } catch (err) {
-        res.status(400).json({ error: err.message });
+        res.status(500).json({ error: err.message });
     }
 });
+
+// Post playlist
+router.post("/", async (req, res) => {
+    try {
+        const newPlaylist = new Playlist(req.body);
+        const savedPlaylist = await newPlaylist.save();
+        res.status(201).json(savedPlaylist);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+})
+
+// Update patch playlist : only renaming adding/removing songs
+router.patch("/:id", async (req, res) => {
+  try {
+      const updatedPlaylist = await Playlist.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        { new: true, runValidators: true }
+        ).populate("songs", "title -_id").populate("user", "username -_id");
+
+        if(!updatedPlaylist) return res.status(404).json({ error: "Playlist not found" });
+        res.json(updatedPlaylist);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+})
+
+// Delete playlist
+router.delete("/:id", async (req, res) => {
+    try {
+        const deletedPlaylist = await Playlist.findByIdAndDelete(req.params.id);
+        if(!deletedPlaylist) return res.status(404).json({ error: "Playlist not found" });
+        res.json({message: "Playlist deleted"});
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+})
 
 export default router;
